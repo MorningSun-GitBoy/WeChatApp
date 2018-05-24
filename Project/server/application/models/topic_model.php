c4ba74e520222970cca82e8f39d32267dd957c74
MorningSun-GitBoy <ShortApp10101@yeah.net>
2018-05-24 13:18:25 +0800

    daily

---
+++ Project/server/application/models/topic_model.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/5/24 0024
+ * Time: 11:01
+ */
+use QCloud_WeApp_SDK\Mysql\Mysql as DB;
+class topic_model extends CI_Model
+{
+    public function getList()
+    {
+
+    }
+}
